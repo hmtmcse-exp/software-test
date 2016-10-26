5c0d6fa1e6fe397b5e83535bb5619fbc237f3556
Touhid Mia <touhid@bitmascot.com>
2016-10-26 14:04:14 +0600

    with driver

--- cucumber-with-selenium-driver/src/test/java/GoogleCalculatorSteps.java
+++ cucumber-with-selenium-driver/src/test/java/GoogleCalculatorSteps.java
@@ -8,11 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
